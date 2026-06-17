8e9d7cde85977b4ea64602b3e1ecaf99bf5980a1
Purushothaman S <purushothamanjerry03@gmail.com>
2026-06-17 10:37:00 +0530

    Updated output format

---
+++ submission/ansible/callbacks/clean_stdout.py
@@ -0,0 +1,33 @@
+from ansible.plugins.callback import CallbackBase
+
+class CallbackModule(CallbackBase):
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'clean_stdout'
+
+    def v2_playbook_on_start(self, playbook):
+        pass
+
+    def v2_playbook_on_play_start(self, play):
+        pass
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        pass
+
+    def v2_runner_on_ok(self, result):
+        if result._task.action == 'debug':
+            msg = result._result.get('msg', '')
+            if isinstance(msg, list):
+                msg = '\n'.join(str(m) for m in msg)
+            self._display.display(f"\033[97m{msg}\033[0m")
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        if not ignore_errors:
+            msg = result._result.get('msg', result._result.get('stderr', 'Task failed'))
+            self._display.display(f"\033[91m[ERROR] {msg}\033[0m")
+
+    def v2_runner_on_unreachable(self, result):
+        self._display.display(f"\033[91m[UNREACHABLE] {result._host.get_name()}\033[0m")
+
+    def v2_playbook_on_stats(self, stats):
+        pass
